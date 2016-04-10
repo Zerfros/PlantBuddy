530189a0bc7b0f21c232d33b6f5d968554304788
Zerfros <tae.zerfros@gmail.com>
2016-04-11 00:41:08 +0700

    Added delay in notification

--- routes/recieveMessage.js
+++ routes/recieveMessage.js
@@ -1,6 +1,8 @@
 var mqtt = require('mqtt');
 var client = mqtt.connect('mqtt://broker.mqttdashboard.com');
 var mongoose = require('mongoose');
+var sendData = true;
+var delay = 1800000;
 
 var db = require('../controller/statusDB');
 // var operation = mongoose.model('operation');
@@ -13,34 +15,45 @@ client.on('connect', function(){
 	});
 })
 
+function timer(){
+	sendData = true;
+}
+
 
 client.on('message', function(topic, message){
 	console.log(message.toString());
-	if (parseInt(message.toString())>80) {
-		var date = new Date();
-		var saveData = new db({process: 'High', time: (date.toISOString())});
-		saveData.save(function(err){
-			if (err){
-				return err;
-			}else{
-				console.log("add success");
-			}
-		})
-		// db.find({}, function(data){
-		// 	console.log(data);
-		// });
-	}else if (parseInt(message.toString())<30) {
-		var date = new Date();
-		var saveData = new db({process: 'Low', time: (date.toISOString())});
-		saveData.save(function(err){
-			if (err){
-				return err;
-			}else{
-				console.log("add success");
-			}
-		})
-		// db.find({}, function(data){
-		// 	console.log(data);
-		// });
-	};
+		if (sendData){
+			sendData = false;
+			if (parseInt(message.toString())>80) {
+				var date = new Date();
+				var saveData = new db({process: 'High', time: (date.toISOString())});
+				saveData.save(function(err){
+					if (err){
+						return err;
+					}else{
+						console.log("add success");
+						setTimeout(timer, delay);
+					}
+				})
+				// db.find({}, function(data){
+				// 	console.log(data);
+				// });
+			}else if (parseInt(message.toString())<30) {
+				var date = new Date();
+				var saveData = new db({process: 'Low', time: (date.toISOString())});
+				saveData.save(function(err){
+					if (err){
+						return err;
+					}else{
+						console.log("add success");
+						setTimeout(timer, 10000)
+					}
+				})
+				// db.find({}, function(data){
+				// 	console.log(data);
+				// });
+			};
+		}else{
+			console.log("function is delay!!")
+		}
 })
